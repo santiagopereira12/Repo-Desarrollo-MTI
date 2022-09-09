ede234634102c24d0b2177bb5c2663d15cba7857
santiago pereira <spereira98@misena.edu.co>
2022-09-09 16:40:03 -0500

    Carpeta escondida

---
+++ NO INGRESAR/Josefino/Josefino.js
@@ -0,0 +1,6 @@
+let Josefino = "<b style= 'color: red'> Karen te quiero demasiado, y estoy completamente orgulloso de ti, sigue así mereces muchas cosas lindas <3 </b>";
+let josefi = "<b>te deseo lo mejor, tal vez nunca sabre nada de lo que pasa, pero sabes bien lo que siento y siempre sera así";
+let josef = "<b style 'color : red'> TE QUIERO DEMASIADO, ERES REALMENTE ESPECIAL Y MERECES GRANDES COSAS :3</b>";
+document.write(Josefino,"<br>");
+document.write(josefi,"<br>");
+document.write(josef);
